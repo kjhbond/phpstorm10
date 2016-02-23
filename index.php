2c6016586667e2b1194afeb386284fd74ccac4b1
kjhbond <kjhbond@gmail.com>
2016-02-24 06:19:20 +0900

    123
    1233333

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kjh
+ * Date: 2016-02-24
+ * Time: 오전 2:31
+ */
+
+function add( $a, $b ){
+    print_r( $a );
+    return $a + $b;
+}
+
+
+add( 3, 3 );
+
+
---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+
+function add( $a, $b ){
+    print_r( $a );
+    return $a + $b;
+}
+
+
+add( 3, 3 );
+
